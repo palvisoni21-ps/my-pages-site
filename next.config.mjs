df35eecb66017294f9f1382a7176524d9f71c53c
Palvi Soni <palvisoni21@gmail.com>
2026-07-25 10:18:18 +0530

    DONE

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",              // build to static HTML in ./out
+  images: { unoptimized: true }, // <Image> without the server optimizer
 };
 
 export default nextConfig;
